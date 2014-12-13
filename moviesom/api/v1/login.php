0414e8cce4208babe379795a7cb552d4f809f524
Willem Liu <willem_liu@hotmail.com>
2014-12-13 01:43:09 +0100

    Implemented login token support

--- moviesom/api/v1/login.php
+++ moviesom/api/v1/login.php
@@ -7,9 +7,6 @@
    *    "password": "thesecret"
    *  }
    */
-
-
-
   header('HTTP/1.1 500 Internal Server Error');
   header('Content-type: application/json');
   $response = [];
@@ -17,14 +14,46 @@
 
   $requestJson = json_decode(file_get_contents("php://input"), true);
   // Credentials should be valid.
-  $credentials->checkCredentials($requestJson['username'], $requestJson['password']);
+  if(isset($requestJson['token']) && strlen($requestJson['token']) > 0) {
+    $credentials->checkLoginToken($requestJson['token'], $_SERVER['REMOTE_ADDR']);
+  } else {
+    $credentials->checkCredentials($requestJson['username'], $requestJson['password']);
+  }
+  
+  $loggedIn = $credentials->hasMoviesomAccess();
   
   // Check if sufficient privileges.
-  if($credentials->hasMoviesomAccess() === false) {
+  if($loggedIn === false) {
     header('HTTP/1.1 401 Unauthorized');
     $response['message'] = 'Insufficient rights';
     $response['status'] = 401;
-  } else {
+  } else {  
+      try {
+        $dbh = $db->connect();
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        if ($dbh->inTransaction() === false) {
+          $dbh->beginTransaction();
+        }
+        $token = hash("sha512", password_hash(mt_rand() . uniqid("moviesomUID_", true), PASSWORD_BCRYPT, array("cost" => 10)));
+        $userId = $credentials->getUserId();
+        $stmt = $dbh->prepare("INSERT login_tokens (user_id, token, ip) VALUES (:user_id, :token, :ip) ON DUPLICATE KEY UPDATE token=:token");
+        $stmt->bindParam(":user_id", $userId);
+        $stmt->bindParam(":token", $token);
+        $stmt->bindParam(":ip", $_SERVER['REMOTE_ADDR']);
+        $stmt->execute();
+        
+        if($dbh->commit()) {
+          header('HTTP/1.1 200 OK');
+          $response['status'] = 200;
+          $response['loginToken'] = $token;
+        } else {
+          $response['message'] = '';
+        }
+      }
+      catch(PDOException $e) {  
+        $response['message'] = $e;
+      }
+  
     header('HTTP/1.1 200 OK');
     $response['status'] = 200;
   }
