eae32a1b153629fb9b2a24a469bea54ff28430b2
Willem Liu <willem_liu@hotmail.com>
2015-01-14 22:55:43 +0100

    Fixed set movie ratings bug

--- moviesom/api/v1/setMovieRatings.php
+++ moviesom/api/v1/setMovieRatings.php
@@ -81,8 +81,10 @@
       $stmt->bindParam(":tmdb_id", $requestJson["tmdb_id"]);
       $stmt->bindParam(":imdb_id", $requestJson["imdb_id"]);
       $stmt->bindParam(":tmdb_rating", $requestJson["tmdb_rating"], PDO::PARAM_STR);
+      $requestJson["imdb_rating"] = ($requestJson["imdb_rating"]) ? $requestJson["imdb_rating"] : 0;
       $stmt->bindParam(":imdb_rating", $requestJson["imdb_rating"], PDO::PARAM_STR);
       $stmt->bindParam(":tmdb_votes", $requestJson["tmdb_votes"], PDO::PARAM_INT);
+      $requestJson["imdb_votes"] = ($requestJson["imdb_votes"]) ? $requestJson["imdb_rating"] : 0;
       $stmt->bindParam(":imdb_votes", $requestJson["imdb_votes"], PDO::PARAM_INT);
       $stmt->execute();
 
