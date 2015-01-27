a23259793d37b81c4f9c49abe12eff5755354cf9
Willem Liu <willem_liu@hotmail.com>
2015-01-27 20:57:09 +0100

    Added crawler for tv series and tv episodes.

--- crawl.php
+++ crawl.php
@@ -8,7 +8,7 @@
     // Update every week Movies older than 1 month but not older than 3 months
     // Update every 2 weeks Movies older than 3 months but not older than 6 months
     // Update every month Movies older than 6 months
-    $stmt = $dbh->prepare("SELECT ms.tmdb_id, title, release_date, updated FROM movie_sources AS ms
+    $stmt = $dbh->prepare("SELECT ms.tmdb_id FROM movie_sources AS ms
                             JOIN movie_ratings AS mr ON mr.source_id=ms.tmdb_id 
                             JOIN movies AS m ON m.id=ms.movie_id 
                           WHERE 
@@ -24,9 +24,58 @@
                                 AND mr.updated < NOW() - INTERVAL 1 MONTH)");
     $stmt->execute();
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-      echo "cd C:/wamp/www/phantomjs-1.9.8-windows & moviesom.bat " . $row["tmdb_id"] . PHP_EOL;
-      echo exec("cd C:/wamp/www/phantomjs-1.9.8-windows & moviesom.bat " . $row["tmdb_id"]) . PHP_EOL;
+      echo "cd C:/wamp/www/phantomjs-1.9.8-windows & moviesom.bat tmdbMovieId " . $row["tmdb_id"] . PHP_EOL;
+      echo exec("cd C:/wamp/www/phantomjs-1.9.8-windows & moviesom.bat tmdbMovieId " . $row["tmdb_id"]) . PHP_EOL;
     }
+    
+    // Update every day TV Series not older than 1 month
+    // Update every week TV Series older than 1 month but not older than 3 months
+    // Update every 2 weeks TV Series older than 3 months but not older than 6 months
+    // Update every month TV Series older than 6 months
+    $stmt = $dbh->prepare("SELECT ts.tmdb_id FROM tv_sources AS ts
+                            JOIN tv_ratings AS tr ON tr.source_id=ts.tmdb_id 
+                            JOIN tv AS tv ON tv.id=ts.tv_id 
+                          WHERE 
+                            (tv.last_air_date > NOW() - INTERVAL 1 MONTH 
+                            AND tr.updated < NOW() - INTERVAL 1 DAY)
+                            OR (tv.last_air_date < NOW() - INTERVAL 1 MONTH 
+                                AND tv.last_air_date > NOW() - INTERVAL 3 MONTH 
+                                AND tr.updated < NOW() - INTERVAL 1 WEEK)
+                            OR (tv.last_air_date < NOW() - INTERVAL 3 MONTH 
+                                AND tv.last_air_date > NOW() - INTERVAL 6 MONTH 
+                                AND tr.updated < NOW() - INTERVAL 2 WEEK)
+                            OR (tv.last_air_date < NOW() - INTERVAL 6 MONTH
+                                AND tr.updated < NOW() - INTERVAL 1 MONTH)");
+    $stmt->execute();
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+      echo "cd C:/wamp/www/phantomjs-1.9.8-windows & moviesom.bat tmdbTvId " . $row["tmdb_id"] . PHP_EOL;
+      echo exec("cd C:/wamp/www/phantomjs-1.9.8-windows & moviesom.bat tmdbTvId " . $row["tmdb_id"]) . PHP_EOL;
+    }
+    
+    // Update every day TV Episode not older than 1 month
+    // Update every week TV Episode older than 1 month but not older than 3 months
+    // Update every 2 weeks TV Episode older than 3 months but not older than 6 months
+    // Update every month TV Episode older than 6 months
+    $stmt = $dbh->prepare("SELECT tes.tmdb_id FROM tv_episode_sources AS tes
+                            JOIN tv_episode_ratings AS ter ON ter.source_id=tes.tmdb_id 
+                            JOIN tv_episodes AS te ON te.id=tes.tv_episode_id 
+                          WHERE 
+                            (te.air_date > NOW() - INTERVAL 1 MONTH 
+                            AND ter.updated < NOW() - INTERVAL 1 DAY)
+                            OR (te.air_date < NOW() - INTERVAL 1 MONTH 
+                                AND te.air_date > NOW() - INTERVAL 3 MONTH 
+                                AND ter.updated < NOW() - INTERVAL 1 WEEK)
+                            OR (te.air_date < NOW() - INTERVAL 3 MONTH 
+                                AND te.air_date > NOW() - INTERVAL 6 MONTH 
+                                AND ter.updated < NOW() - INTERVAL 2 WEEK)
+                            OR (te.air_date < NOW() - INTERVAL 6 MONTH
+                                AND ter.updated < NOW() - INTERVAL 1 MONTH)");
+    $stmt->execute();
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+      echo "cd C:/wamp/www/phantomjs-1.9.8-windows & moviesom.bat tmdbTvId " . $row["tmdb_id"] . PHP_EOL;
+      echo exec("cd C:/wamp/www/phantomjs-1.9.8-windows & moviesom.bat tmdbTvId " . $row["tmdb_id"]) . PHP_EOL;
+    }
+    
   }
   catch(PDOException $e) {  
     echo $e . PHP_EOL;
