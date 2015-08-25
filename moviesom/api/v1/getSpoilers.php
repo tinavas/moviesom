d3c2ba039a02a8156066a04accd5a78f12ab2267
Willem Liu <willem_liu@hotmail.com>
2015-08-25 22:26:45 +0200

    83 Able to add note to Recommendation for connections to see
    Now sends e-mail to writer of spoiler when read for the first time
    Also rewards writer with 10 points

--- moviesom/api/v1/getSpoilers.php
+++ moviesom/api/v1/getSpoilers.php
@@ -37,11 +37,11 @@
     try {
       $dbh = $db->connect();
       $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      $stmt = $dbh->prepare("SELECT u.username, rm.tmdb_id, rm.spoiler FROM recommend_movies AS rm 
+      $stmt = $dbh->prepare("SELECT rm.id, u.username, rm.spoiler FROM recommend_movies AS rm 
                                 JOIN users AS u ON rm.recommend_by=u.id
                               WHERE rm.tmdb_id=:tmdb_id
                                 AND (rm.spoiler IS NOT NULL AND rm.spoiler<>'')
-                                AND (rm.recommend_to=:user_id OR rm.recommend_by=:user_id) GROUP BY recommend_by");
+                                AND (rm.recommend_to=:user_id OR rm.recommend_by=:user_id) GROUP BY rm.recommend_by ORDER BY rm.added DESC");
       $stmt->bindValue(":tmdb_id", $requestJson['tmdb_id']);
       $stmt->bindParam(":user_id", $userId);
       $stmt->execute();
---
+++ moviesom/api/v1/readSpoiler.php
@@ -0,0 +1,76 @@
+<?php
+  /**
+   * Read spoiler.
+   * Expects JSON as payload I.e.:
+   *  {
+   *    "token": "d967a19940bdc4d498d0420a9fb12802ab5857a0a634ab73ae8984c5cf46ab3f9322dd5c1c3f069cc9d226ce47112747976c289cf6ae7b41a8ac72a7dc69c83f",
+   *    "recommend_id": 403
+   *  }
+   */
+
+  header('HTTP/1.1 500 Internal Server Error');
+  header('Content-type: application/json');
+  $response = [];
+  $response['status'] = 500;
+  
+  $requestJson = json_decode(file_get_contents("php://input"), true);
+
+  // Login token should be valid.
+  if(isset($requestJson['token']) && strlen($requestJson['token']) > 0) {
+    $credentials->checkLoginToken($requestJson['token']);
+  }
+  
+  $loggedIn = $credentials->hasMoviesomAccess();
+  $userId = $credentials->getUserId();
+  
+  if($loggedIn === false) {
+    header('HTTP/1.1 401 Unauthorized');
+    $response['message'] = 'Insufficient rights';
+    $response['status'] = 401;
+  } else if (isset($requestJson['recommend_id'])) {
+    try {
+      $dbh = $db->connect();
+      $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      if ($dbh->inTransaction() === false) {
+        $dbh->beginTransaction();
+      }
+    
+      $stmt = $dbh->prepare("SELECT m.title, ms.tmdb_id, rm.spoiler, rm.recommend_by,
+                                (SELECT username FROM users WHERE id=rm.recommend_by) AS mail_from, 
+                                (SELECT username FROM users WHERE id=rm.recommend_to) AS mail_to 
+                              FROM recommend_movies AS rm
+                                JOIN movie_sources AS ms ON ms.tmdb_id=rm.tmdb_id
+                                JOIN movies AS m ON m.id=ms.movie_id
+                              WHERE rm.id=:recommend_id AND rm.recommend_by=:user_id AND rm.is_spoiler_read=0");
+      $stmt->bindParam(":recommend_id", $requestJson["recommend_id"]);
+      $stmt->bindParam(":user_id", $userId);
+      $stmt->execute();
+      
+      $title = "";
+      while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $stmt2 = $dbh->prepare("UPDATE recommend_movies SET is_spoiler_read=1 WHERE id=:recommend_id AND recommend_by=:user_id AND is_spoiler_read=0");
+        $stmt2->bindParam(":recommend_id", $requestJson["recommend_id"]);
+        $stmt2->bindParam(":user_id", $userId);
+        $stmt2->execute();
+
+        // Add points for recommender for spoiling the movie.
+        $points = 10;
+        $credentials->addPoints($row['recommend_by'], $points);
+        // Send spoiler read mail.
+        $movieSomMail->mailSpoilerReadPoints($row["mail_from"], $row["mail_to"], $row["tmdb_id"], $row["title"], $row["spoiler"], $points);
+        break;
+      }
+      
+      if($dbh->commit()) {
+        header('HTTP/1.1 200 OK');
+        $response['status'] = 200;
+      } else {
+        $response['message'] = '';
+      }
+    }
+    catch(PDOException $e) {  
+      $response['message'] = $e;
+    }
+  }
+  
+?>
